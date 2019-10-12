4c56004631b7f2da25c5a1843afa36f71fc43956
Prakhar812 <54809977+Prakhar812@users.noreply.github.com>
2019-10-12 12:03:48 +0100

    Use_of_macros.cpp

---
+++ use_of_macros.cpp
@@ -0,0 +1,21 @@
+//use of macros save time while writing code
+
+
+#define ll long long 
+#define "\n" endl
+#define 1000000007 m
+
+
+int main()
+{
+ll t;
+cin>>t;
+
+  while(t--)
+  {
+    ll n;
+    cin>>n;
+    ll value=n*n;
+    cout<<n<<endl;
+    }
+}
