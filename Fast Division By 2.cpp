da4c5f448845c099056bd3d166ce1e115b973eba
ChandanSitlani <40667874+ChandanSitlani@users.noreply.github.com>
2019-10-12 16:10:27 +0530

    Create Fast Division By 2.cpp

---
+++ Fast Division By 2.cpp
@@ -0,0 +1,13 @@
+//To divide fast by 2 use leftshift and right shift
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int a;
+cin>>a;
+int mult=a<<2;
+int div=a>>2;
+cout<<"Divided by 2:"<<div
+cout<<endl<<"Multiplied by 2:"<<mult;
+}
